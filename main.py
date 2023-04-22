c0f986014143f3fd3dfe237c3def10be7dba0218
Joel Ermantraut <joelermantraut@gmail.com>
2023-04-22 03:00:37 -0300

    Minor changes

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class MainWindow(ModelWindow):
     def __init__(self, db_manager, title, styles=None):
         super().__init__(db_manager, title, styles)
 
+        self.windows = list()
         self.title = title
 
         self.init_UI()
